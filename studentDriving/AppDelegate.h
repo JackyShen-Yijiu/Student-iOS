09750c84f321c11bc51b98d17211646c383a711d
Mortime <Mortimey@163.com>
2015-11-15 10:14:37 +0800

    添加一步商城模块

--- studentDriving/AppDelegate.h
+++ studentDriving/AppDelegate.h
@@ -12,6 +12,7 @@
 @interface AppDelegate : UIResponder <UIApplicationDelegate,BMKGeneralDelegate>{
     BMKMapManager *_mapManager;
 }
+
 @property (strong, nonatomic) UIWindow *window;
 @property (nonatomic,assign)BOOL allowRotation;
 
