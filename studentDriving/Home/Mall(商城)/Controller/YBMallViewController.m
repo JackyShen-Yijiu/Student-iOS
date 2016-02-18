9f9e6857d90c0b155ecc6a32b5793c8642473b04
Mortime <Mortimey@163.com>
2016-02-18 11:31:10 +0800

    商城UI添加

--- studentDriving/Home/Mall(商城)/Controller/YBMallViewController.m
+++ studentDriving/Home/Mall(商城)/Controller/YBMallViewController.m
@@ -7,33 +7,48 @@
 //
 
 #import "YBMallViewController.h"
+#import "YBIntegralMallCell.h"
 
-@interface YBMallViewController ()
-
+@interface YBMallViewController ()<UITableViewDelegate,UITableViewDataSource>
+@property (nonatomic, strong) UITableView *tabelView;
 @end
 
 @implementation YBMallViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-
     self.title = @"商城";
-
+    self.tabelView.delegate = self;
+    self.tabelView.dataSource  = self;
+    [self.view addSubview:self.tabelView];
 }
-
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section{
+    return 10;
+}
+- (CGFloat)tableView:(UITableView *)tableView heightForRowAtIndexPath:(NSIndexPath *)indexPath{
+    return 165;
+}
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath{
+    NSString *cellID =@"MallCellID";
+    YBIntegralMallCell *mallCell = [tableView dequeueReusableCellWithIdentifier:cellID];
+    if (!mallCell) {
+        mallCell = [[YBIntegralMallCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:cellID];
+        mallCell.backgroundColor = [UIColor clearColor];
+    }
+    return mallCell;
+}
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
 }
 
-/*
-#pragma mark - Navigation
+- (UITableView *)tabelView{
+    if (_tabelView == nil) {
+        _tabelView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height - 65) style:UITableViewStylePlain];
+        _tabelView.backgroundColor = [UIColor clearColor];
+        _tabelView.separatorStyle = UITableViewCellSeparatorStyleNone;
+    }
+    return _tabelView;
 
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
 }
-*/
-
 @end
---
+++ studentDriving/LoginViewController/YBIntegralMallCell.h
@@ -0,0 +1,13 @@
+//
+//  YBIntegralMallCell.h
+//  studentDriving
+//
+//  Created by zyt on 16/2/18.
+//  Copyright © 2016年 jatd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YBIntegralMallCell : UITableViewCell
+
+@end
