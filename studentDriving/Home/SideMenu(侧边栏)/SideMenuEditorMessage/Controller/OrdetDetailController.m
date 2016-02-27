a729923e7dbbc8bdfe75d42a5b460f2e6738536d
Mortime <Mortimey@163.com>
2016-02-27 20:04:03 +0800

    订单详情模块

---
+++ studentDriving/Home/SideMenu(侧边栏)/SideMenuEditorMessage/Controller/OrdetDetailController.m
@@ -0,0 +1,37 @@
+//
+//  OrdetDetailController.m
+//  studentDriving
+//
+//  Created by zyt on 16/2/27.
+//  Copyright © 2016年 jatd. All rights reserved.
+//
+
+#import "OrdetDetailController.h"
+
+@interface OrdetDetailController ()
+
+@end
+
+@implementation OrdetDetailController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+   
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
