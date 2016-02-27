f288622ed443da91b93245a111780ae122b43e59
Mortime <Mortimey@163.com>
2016-02-27 15:58:00 +0800

    兑换成功后的跳转

---
+++ studentDriving/Home/Mall(商城)/Controller/MallListSuccessController.h
@@ -0,0 +1,14 @@
+//
+//  MallListSuccessController.h
+//  studentDriving
+//
+//  Created by zyt on 16/2/27.
+//  Copyright © 2016年 jatd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MallListSuccessController : UIViewController
+@property (nonatomic, strong) NSString *successUrl;
+
+@end
