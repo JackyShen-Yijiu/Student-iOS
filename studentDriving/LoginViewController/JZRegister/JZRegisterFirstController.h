3b228ef69ded790b7c7667cd6fe796ab2e530bd2
yjg2014 <18717357779@163.com>
2016-03-04 17:11:42 +0800

    JZ注册控制器添加

---
+++ studentDriving/LoginViewController/JZRegister/JZRegisterFirstController.h
@@ -0,0 +1,13 @@
+//
+//  JZRegisterFirstController.h
+//  studentDriving
+//
+//  Created by ytzhang on 16/3/4.
+//  Copyright © 2016年 jatd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JZRegisterFirstController : UIViewController
+
+@end
---
+++ studentDriving/LoginViewController/JZRegister/JZRegisterFirstController.m
@@ -0,0 +1,22 @@
+//
+//  JZRegisterFirstController.m
+//  studentDriving
+//
+//  Created by ytzhang on 16/3/4.
+//  Copyright © 2016年 jatd. All rights reserved.
+//
+
+#import "JZRegisterFirstController.h"
+
+@interface JZRegisterFirstController ()
+
+
+@end
+
+@implementation JZRegisterFirstController
+- (void)viewDidLoad{
+    self.view.backgroundColor = [UIColor whiteColor];
+    self.title = @"注册";
+    
+}
+@end
