0596b3b350c38a0a4a3dd2a0084e07c6af9ab1d4
yjg2014 <18717357779@163.com>
2016-03-05 00:09:33 +0800

    pull

---
+++ studentDriving/Home/Login/YBLoginController.h
@@ -0,0 +1,13 @@
+//
+//  YBLoginController.h
+//  studentDriving
+//
+//  Created by 大威 on 16/3/4.
+//  Copyright © 2016年 jatd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YBLoginController : UIViewController
+
+@end
