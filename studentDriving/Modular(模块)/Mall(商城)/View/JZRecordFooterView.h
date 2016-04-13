aaa74509d92cf7faf3ee79f49d1a7a8599935552
yjg2014 <18717357779@163.com>
2016-04-13 11:46:38 +0800

    商城模块数据联调

--- studentDriving/Modular(模块)/Mall(商城)/View/JZRecordFooterView.h
+++ studentDriving/Modular(模块)/Mall(商城)/View/JZRecordFooterView.h
@@ -7,7 +7,22 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "JZRecordOrdrelist.h"
+#import "YBIntegralMallModel.h"
 
 @interface JZRecordFooterView : UIView
+
+@property (nonatomic, strong) UILabel *titleLable;
+
+@property (nonatomic, strong) UILabel *addressLabel;
+
+@property (nonatomic, strong) UIImageView *codeImageView;
+
+@property (nonatomic, strong) UILabel *stateLable;
+
+
+- (instancetype)initWithFrame:(CGRect)frame recordOrderModel:(JZRecordOrdrelist *)recordOrderModel;
+
+- (instancetype)initWithFrame:(CGRect)frame integralMallModel:(YBIntegralMallModel *)integralMallModel formMall:(BOOL)formMall codeStr:(NSString *)codeStr;
 
 @end
