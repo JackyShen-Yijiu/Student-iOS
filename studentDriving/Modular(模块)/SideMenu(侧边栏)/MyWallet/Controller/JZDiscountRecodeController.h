36d5085372471469ba35fcf4a3cc40c422a5bde9
likegoto <leikai0391@163.com>
2016-04-15 19:04:02 +0800

    钱包中我的优化券使用记录界面添加

---
+++ studentDriving/Modular(模块)/SideMenu(侧边栏)/MyWallet/Controller/JZDiscountRecodeController.h
@@ -0,0 +1,13 @@
+//
+//  JZDiscountRecodeController.h
+//  studentDriving
+//
+//  Created by 雷凯 on 16/4/15.
+//  Copyright © 2016年 jatd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface JZDiscountRecodeController : UIViewController
+
+@end
