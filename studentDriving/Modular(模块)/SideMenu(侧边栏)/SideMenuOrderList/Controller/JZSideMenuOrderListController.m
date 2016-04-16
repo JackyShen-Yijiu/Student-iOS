5a36986dbb8a10fadabaa9e766a03073d232bca9
yjg2014 <18717357779@163.com>
2016-04-16 15:47:43 +0800

    占位图片更换

--- studentDriving/Modular(模块)/SideMenu(侧边栏)/SideMenuOrderList/Controller/JZSideMenuOrderListController.m
+++ studentDriving/Modular(模块)/SideMenu(侧边栏)/SideMenuOrderList/Controller/JZSideMenuOrderListController.m
@@ -11,6 +11,7 @@
 #import "YBOrderListViewController.h"
 #import "JZSideMenuOrderDiscountView.h"
 #import "JZNoDataShowBGView.h"
+#import "DVVNoDataPromptView.h"
 
 #define HeaderH 40
 
@@ -25,6 +26,8 @@
 @property (nonatomic, strong) YBOrderListViewController *signUpVC;
 
 @property (nonatomic, strong) JZSideMenuOrderDiscountView *sideMenuOrderDiscountView;
+
+@property (nonatomic, strong) DVVNoDataPromptView *noCountentView;
 @end
 
 @implementation JZSideMenuOrderListController
@@ -123,8 +126,15 @@
 }
 #pragma mark --- 兑换劵无数据的占位图片
 - (void)initWithNoDataOrderLsitDiscountBG{
-    JZNoDataShowBGView *noData = [[JZNoDataShowBGView alloc] initWithFrame:CGRectMake(kSystemWide, 0, self.scrollView.width, self.scrollView.height)];
-    [self.scrollView addSubview:noData];
+    
+    
+    // 添加空白占位图片
+    self.noCountentView = [[DVVNoDataPromptView alloc] initWithTitle:@"一大波优惠活动正在来袭" image:[UIImage imageNamed:@"gift"] subTitle:@"敬请期待"];
+    self.noCountentView.titleLabel.textColor = JZ_FONTCOLOR_DRAK;
+    self.noCountentView.subTitleLabel.textColor = JZ_FONTCOLOR_DRAK;
+    self.noCountentView.titleLabel.font = [UIFont systemFontOfSize:14];
+    self.noCountentView.subTitleLabel.font = [UIFont systemFontOfSize:14];
+    [self.scrollView addSubview:self.noCountentView];
 }
 
 #pragma mark --- Lazy
